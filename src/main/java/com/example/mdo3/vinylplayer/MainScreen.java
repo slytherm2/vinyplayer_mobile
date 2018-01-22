d2b71f2d45c4d784f240c343bdd3525c5165a4a8
Martin Do <martin14do@gmail.com>
2018-01-21 23:11:56 -0500

    main screen

--- src/main/java/com/example/mdo3/vinylplayer/MainScreen.java
+++ src/main/java/com/example/mdo3/vinylplayer/MainScreen.java
@@ -1,7 +1,14 @@
 package com.example.mdo3.vinylplayer;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        //get the intent of the previous activity
+        Intent intent = getIntent();
+
+        //get the user email from the previous activity (login/signup)
+        String user = null;
+        if(Login.LOGIN_USER == null)
+            user = intent.getStringExtra(SignUp.SIGNUP_USER);
+        else
+            user = intent.getStringExtra(Login.LOGIN_USER);
+
+        //creates a new title with "welcome" and "user email before the @"
+        String tempStr = user.substring(0,user.indexOf('@'));
+        String newTitle = getString(R.string.label_Welcome) + tempStr;
+
+        //update title to reflect user "welcome ...user"
+        TextView title = (TextView) findViewById(R.id.main_title);
+        title.setText(newTitle);
+
+        Button btn = findViewById(R.id.main_stateBTN);
+        btn.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
     }
 }
